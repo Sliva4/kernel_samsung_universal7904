3829c699d093f8324d4c344f2c730b5ce2570936
vpfaib <vpfaib@MacBook-Air-vpfaib.local>
2025-10-06 15:13:20 +0700

    MacOS Build and My Features Test

---
+++ macos.sh
@@ -0,0 +1,5 @@
+CPLUS_INCLUDE_PATH=/Users/vpfaib/Downloads/aarch64-unknown-linux-gnu/include
+export CPLUS_INCLUDE_PATH
+C_INCLUDE_PATH=/Users/vpfaib/Downloads/aarch64-unknown-linux-gnu/include
+export C_INCLUDE_PATH
+./build_kernel a30s no
