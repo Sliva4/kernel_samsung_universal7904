4ed92faea2fc1176d0e086189cc1ec07545a72b4
Sliva4 <slivaktoto@gmail.com>
2025-10-06 16:53:44 +0700

    Update macos.sh
    Signed-off-by: Sliva4 <slivaktoto@gmail.com>

--- macos.sh
+++ macos.sh
@@ -2,4 +2,5 @@ CPLUS_INCLUDE_PATH=/Users/vpfaib/Downloads/aarch64-unknown-linux-gnu/include
 export CPLUS_INCLUDE_PATH
 C_INCLUDE_PATH=/Users/vpfaib/Downloads/aarch64-unknown-linux-gnu/include
 export C_INCLUDE_PATH
+export PATH="/opt/homebrew/opt/gnu-sed/libexec/gnubin:$PATH"
 ./build_kernel a30s no
